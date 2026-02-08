ad0ebdaa5e04b5761bd4a384b957e3e0dac8b536
Toufique <toufiquee92@gmail.com>
2026-02-08 19:10:50 +0600

    loop and ternary operator

---
+++ PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (num < 2) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        String result = isPrime ? "is a Prime number" : "is not a Prime number";
+        System.out.println(num + " " + result);
+
+    }
+}
