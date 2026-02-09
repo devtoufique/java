2532060ed2d91cc14c72d540e45cf47dd8b026b6
Toufique <toufiquee92@gmail.com>
2026-02-09 13:38:14 +0600

    Add Goldbach's conjecture program to find partitions of even numbers

---
+++ Goldbach.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Goldbach {
+    // Helper method to check if a number is prime
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an even number greater than 2: ");
+        int num = sc.nextInt();
+
+        if (num <= 2 || num % 2 != 0) {
+            System.out.println("Enter an even number greater than 2.");
+        } else {
+            boolean found = false;
+            for (int i = 2; i <= num / 2; i++) {
+                if (isPrime(i) && isPrime(num - i)) {
+                    System.out.println(num + " = " + i + " + " + (num - i));
+                    found = true;
+                }
+            }
+            if (!found) {
+                System.out.println("No Goldbach partition found (unexpected for even numbers > 2).");
+            }
+        }
+        sc.close();
+    }
+}
