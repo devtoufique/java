d6385cd2b54ee017f6113978c8100a77297756e5
Toufique <toufiquee92@gmail.com>
2026-02-10 17:48:28 +0600

    Add program to print Pascal's Triangle

---
+++ PascalsTriangle.java
@@ -0,0 +1,37 @@
+public class PascalsTriangle {
+
+    public static void printCenteredPascalsTriangle(int n) {
+
+        int[][] triangle = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            triangle[i][0] = 1;
+            triangle[i][i] = 1;
+
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int space = 0; space < n - i - 1; space++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(triangle[i][j] + "   ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        scanner.close();
+
+        printCenteredPascalsTriangle(rows);
+    }
+}
